c07a043cd30d4bd99112486c7f53869011f2da0a
Josh Hartman <jhartman3167@stu.d214.org>
2023-04-24 14:55:53 -0500

    added image/logo to home screen

--- ContentView.swift
+++ ContentView.swift
@@ -2,27 +2,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationView {
+        VStack{
+            ZStack{
+                RoundedRectangle(cornerRadius: 20)
+                    .frame(width: 250, height: 250)
+                    .foregroundColor(.blue)
+                VStack{
+                    Image(systemName: "car.side.front.open.fill")
+                        .scaleEffect(6)
+                        .foregroundColor(.black)
+                        .padding(20)
+                    Text("Mechanic Finder")
+                        .foregroundColor(.black)
+                        .font(.title)
+                        .padding(20)
+                }
+            }
             
             Button {
                 MechanicSearchView()
             } label: {
                 Text("Find a Mechanic")
-                    .frame(width: 150, height: 75)
+                    .frame(width: 250, height: 75)
+                    .background(.blue)
                     .clipShape(Capsule())
-                    .foregroundColor(.blue)
+                    .foregroundColor(.black)
             }
             
             Button {
                 ShoppingCartView()
             } label: {
                 Text("My Shopping Cart")
-                    .frame(width: 150, height: 75)
+                    .frame(width: 250, height: 75)
+                    .background(.blue)
                     .clipShape(Capsule())
-                    .foregroundColor(.blue)
+                    .foregroundColor(.black)
             }
-            
-            
         }
     }
 }
