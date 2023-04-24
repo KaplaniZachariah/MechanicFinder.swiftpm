95d851a533d131e86cbff2b1b675508f8cdc26ab
KaplaniZachariah <zkaplani4201@stu.d214.org>
2023-04-24 14:31:27 -0500

    added the shopping cart view to the file

---
+++ ShoppingCartView.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingCartView.swift
+//  MechanicFinder
+//
+//  Created by Zach kaplani  on 4/24/23.
+//
+
+import SwiftUI
+
+
+struct ShoppingCartView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
