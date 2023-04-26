f44ba61ff4de2309ccd5918dc3a25d1d565effd6
Michael Grabowy <mgrabowy3150@stu.d214.org>
2023-04-26 14:39:51 -0500

    MY FIRST COMMIT RAAAAAHHHHHHH

--- ToolNavigatorView.swift
+++ ToolNavigatorView.swift
@@ -8,9 +8,12 @@
 import SwiftUI
 
 struct ToolNavigatorView: View {
+    @State var ToolSearch: String
     var body: some View{
         VStack{
             NavigationView(){
+                TextField("Enter Tool Here", text: $ToolSearch)
+                    .foregroundColor(.blue)
                 Button("Add to Cart") {
                     
                 }
