bc83ba857770c87f69ea56532b440a1aa6c0aeca
Josh Hartman <jhartman3167@stu.d214.org>
2023-04-26 14:33:48 -0500

    made the buttons into navigation links so they actually bring the user to a new view

--- ContentView.swift
+++ ContentView.swift
@@ -2,41 +2,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-            ZStack{
-                RoundedRectangle(cornerRadius: 20)
-                    .frame(width: 250, height: 250)
-                    .foregroundColor(.blue)
-                VStack{
-                    Image(systemName: "car.side.front.open.fill")
-                        .scaleEffect(6)
-                        .foregroundColor(.black)
+        NavigationView{
+            VStack{
+                ZStack{
+                    RoundedRectangle(cornerRadius: 20)
+                        .frame(width: 250, height: 250)
+                        .foregroundColor(.blue)
                         .padding(20)
-                    Text("Mechanic Finder")
+                    VStack{
+                        Image(systemName: "car.side.front.open.fill")
+                            .scaleEffect(6)
+                            .foregroundColor(.black)
+                            .padding(20)
+                        Text("Mechanic Finder")
+                            .foregroundColor(.black)
+                            .font(.title)
+                            .padding(20)
+                    }
+                }
+                
+                NavigationLink { 
+                    MechanicSearchView()
+                } label: { 
+                    Text("Find a Mechanic")
+                        .font(.title2)
+                        .frame(width: 300, height: 75)
+                        .background(.blue)
+                        .clipShape(Capsule())
                         .foregroundColor(.black)
-                        .font(.title)
                         .padding(20)
                 }
-            }
-            
-            Button {
-                MechanicSearchView()
-            } label: {
-                Text("Find a Mechanic")
-                    .frame(width: 250, height: 75)
-                    .background(.blue)
-                    .clipShape(Capsule())
-                    .foregroundColor(.black)
-            }
-            
-            Button {
-                ShoppingCartView()
-            } label: {
-                Text("My Shopping Cart")
-                    .frame(width: 250, height: 75)
-                    .background(.blue)
-                    .clipShape(Capsule())
-                    .foregroundColor(.black)
+
+                
+                NavigationLink { 
+                    ShoppingCartView()
+                } label: { 
+                    Text("Shop for Tools")
+                        .font(.title2)
+                        .frame(width: 300, height: 75)
+                        .background(.blue)
+                        .clipShape(Capsule())
+                        .foregroundColor(.black)
+                        .padding(20)
+                }
             }
         }
     }
