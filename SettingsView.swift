ed148a9b3651e7298bfac9bd5bdec24f7b7e309c
Michael Grabowy <mgrabowy3150@stu.d214.org>
2023-05-02 14:05:32 -0500

    Made a settings page

---
+++ SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  MechanicFinder
+//
+//  Created by Michael Grabowy on 5/2/23.
+//
+
+
+
+import SwiftUI
+
+struct SettingsView: View {
+    
+    
+    var body: some View {
+        Text("filler")
+    }
+}
